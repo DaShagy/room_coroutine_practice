e7e09e5943e71f375f2aab29ac515c20051f2a71
Juan Jose Abuin <juanj.abuin@gmail.com>
2021-10-06 16:31:38 -0300

    Added recyclerview

--- app/src/main/java/com/dashagy/roomcoroutinespractice/presentation/MainActivity.kt
+++ app/src/main/java/com/dashagy/roomcoroutinespractice/presentation/MainActivity.kt
@@ -6,11 +6,14 @@ import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.RecyclerView
 import com.dashagy.roomcoroutinespractice.R
 import com.dashagy.roomcoroutinespractice.data.NotesDatabase
 import com.dashagy.roomcoroutinespractice.data.repositories.NoteRepositoryImpl
+import com.dashagy.roomcoroutinespractice.domain.entities.Note
 import com.dashagy.roomcoroutinespractice.domain.usecases.GetNotesUseCase
 import com.dashagy.roomcoroutinespractice.domain.usecases.InsertNoteUseCase
+import com.dashagy.roomcoroutinespractice.presentation.adapters.NoteListAdapter
 import com.dashagy.roomcoroutinespractice.presentation.viewmodel.NoteViewModel
 import com.dashagy.roomcoroutinespractice.presentation.viewmodel.NoteViewModelFactory
 
@@ -25,30 +28,36 @@ class MainActivity : AppCompatActivity() {
     private val viewModelFactory by lazy { NoteViewModelFactory(insertNoteUseCase, getNotes) }
     private lateinit var viewModel: NoteViewModel
 
+    private lateinit var adapter: NoteListAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         viewModel = ViewModelProvider(this, viewModelFactory).get(NoteViewModel::class.java)
 
+        adapter = NoteListAdapter(mutableListOf())
+
+        val recyclerView = findViewById<RecyclerView>(R.id.notesList)
+        recyclerView.adapter = adapter
+        recyclerView.setHasFixedSize(true)
+
         val titleEditText = findViewById<EditText>(R.id.editTextTitle)
         val contentEditText = findViewById<EditText>(R.id.editTextContent)
         val insertNoteButton = findViewById<Button>(R.id.btnInsertNote)
-        val noteList = findViewById<TextView>(R.id.textView)
 
-        viewModel.notesList.observe(this) {
-            var toShow  = ""
-            it.forEach { note ->
-                toShow += "${note.title}:\t${note.content}\n"
-            }
-            noteList.text = toShow
-        }
+        viewModel.notesList.observe(this, ::updateUI)
 
         insertNoteButton.setOnClickListener{
             onClickInsertNote(titleEditText.text.toString(), contentEditText.text.toString())
             titleEditText.setText("")
             contentEditText.setText("")
         }
+    }
+
+    private fun updateUI(list: List<Note>) {
+        adapter.updateAdapterDataset(list as MutableList<Note>)
+        adapter.notifyDataSetChanged()
     }
 
     private fun onClickInsertNote(title: String, content: String) {
---
+++ app/src/main/java/com/dashagy/roomcoroutinespractice/presentation/adapters/NoteListAdapter.kt
@@ -0,0 +1,50 @@
+package com.dashagy.roomcoroutinespractice.presentation.adapters
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.RecyclerView
+import com.dashagy.roomcoroutinespractice.R
+import com.dashagy.roomcoroutinespractice.domain.entities.Note
+
+
+class NoteListAdapter(
+    private var dataset: MutableList<Note>
+) : RecyclerView.Adapter<NoteListAdapter.NoteViewHolder>() {
+
+    fun updateAdapterDataset(data: MutableList<Note>){
+        dataset = data
+    }
+
+    inner class NoteViewHolder(view: View) : RecyclerView.ViewHolder(view){
+        val textView: TextView = view.findViewById(R.id.title)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NoteViewHolder {
+
+        val view = LayoutInflater.from(parent.context)
+            .inflate(R.layout.notes_list_recycler_view, parent, false)
+
+        return NoteViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: NoteViewHolder, position: Int) {
+        holder.textView.text = dataset[position].title
+    }
+
+    override fun getItemCount() = dataset.size
+
+    companion object {
+        private val LIST_COMPARATOR = object : DiffUtil.ItemCallback<Note>() {
+            override fun areItemsTheSame(oldItem: Note, newItem: Note): Boolean {
+                return oldItem === newItem
+            }
+
+            override fun areContentsTheSame(oldItem: Note, newItem: Note): Boolean {
+                return oldItem.hashCode() == newItem.hashCode()
+            }
+        }
+    }
+}
