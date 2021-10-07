11b03f777b0272d046430b8cfa4924c192baba26
Juan Jose Abuin <juanj.abuin@gmail.com>
2021-10-07 16:04:11 -0300

    Added edit note implementation

--- app/src/main/java/com/dashagy/roomcoroutinespractice/presentation/viewmodel/NoteViewModel.kt
+++ app/src/main/java/com/dashagy/roomcoroutinespractice/presentation/viewmodel/NoteViewModel.kt
@@ -2,35 +2,58 @@ package com.dashagy.roomcoroutinespractice.presentation.viewmodel
 
 import androidx.lifecycle.*
 import com.dashagy.roomcoroutinespractice.domain.entities.Note
+import com.dashagy.roomcoroutinespractice.domain.usecases.GetNoteByIdUseCase
 import com.dashagy.roomcoroutinespractice.domain.usecases.GetNotesUseCase
 import com.dashagy.roomcoroutinespractice.domain.usecases.InsertNoteUseCase
-import kotlinx.coroutines.flow.Flow
+import com.dashagy.roomcoroutinespractice.presentation.util.NoteState
 import kotlinx.coroutines.launch
 
 class NoteViewModel(
     private val insertNote: InsertNoteUseCase,
-    private val getNotes: GetNotesUseCase
+    private val getNotes: GetNotesUseCase,
+    private val getNoteById: GetNoteByIdUseCase
 ) : ViewModel() {
 
+    private var _state : MutableLiveData<NoteState> = MutableLiveData()
+    val state get() = _state as LiveData<NoteState>
+
     private var _noteTitle : MutableLiveData<String> = MutableLiveData()
+    val noteTitle get() = _noteTitle as LiveData<String>
 
     private var _noteContent: MutableLiveData<String> = MutableLiveData()
+    val noteContent get() = _noteContent as LiveData<String>
+
+    private var noteId: Int? = null
 
     val notesList: LiveData<List<Note>> = getNotes().asLiveData()
 
-    fun update(title: String, content: String){
+    fun update(title: String, content: String, id: Int? = null){
         _noteTitle.value = title
         _noteContent.value = content
+        noteId = id
     }
 
-    fun insertNoteIntoDb() = viewModelScope.launch {
+    fun insertNoteIntoDb(title: String, content: String) = viewModelScope.launch {
+        updateState(NoteState.CreateNoteState)
+        update(title, content, noteId)
         insertNote(
             Note(
                 title = _noteTitle.value!!,
                 content = _noteContent.value!!,
                 timestamp = System.currentTimeMillis(),
-                id = null
+                id = noteId
             )
         )
+    }
+
+    fun getNoteToEdit(id: Int) = viewModelScope.launch {
+        getNoteById(id)?.let {
+            updateState(NoteState.EditNoteState)
+            update(it.title, it.content, it.id)
+        }
+    }
+
+    fun updateState(noteState: NoteState) {
+        _state.value = noteState
     }
 }
